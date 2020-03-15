b17c61d30c3fb9364f4b52f037fb7d75617f5aaf
Chris\Laptop <chris.m.clarke@live.co.uk>
2020-03-15 10:13:03 -0700

    table view

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -6,6 +6,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatInputModule } from "@angular/material/input";
+import { MatSortModule } from "@angular/material/sort";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [BreadcrumbComponent],
@@ -16,13 +22,25 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSortModule,
+    MatIconModule
   ],
   exports: [
     BreadcrumbComponent,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSortModule,
+    MatIconModule
   ]
 })
 export class ComponentsModule {}
