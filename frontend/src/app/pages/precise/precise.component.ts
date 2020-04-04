5fe155ce3ad86aea2de956478ebdc805184fe62f
Chris\Laptop <chris.m.clarke@live.co.uk>
2020-04-04 15:01:10 -0700

    refactor precise home page

--- frontend/src/app/pages/precise/precise.component.ts
+++ frontend/src/app/pages/precise/precise.component.ts
@@ -1,33 +1,59 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ChangeDetectionStrategy,
+} from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CommonStore } from "src/app/stores/common.store";
 import { IODkTableRowData } from "src/app/types/odk.types";
+import { MatPaginator } from "@angular/material/paginator";
+import { PreciseStore, IParticipantSummary } from "src/app/stores";
+import { MatTableDataSource } from "@angular/material/table";
 
+/**
+ * Homepage of precise project
+ * TODO - move table/list display to odk component
+ */
 @Component({
   selector: "app-precise-home",
   templateUrl: "./precise.component.html",
-  styleUrls: ["./precise.component.scss"]
+  styleUrls: ["./precise.component.scss"],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PreciseHomeComponent {
-  tableColumns: string[] = [
-    "f2a_participant_id",
-    "f2a_full_name",
-    "f2a_national_id",
-    "f2a_hdss",
-    "f2a_phone",
-    "f2a_phone_number",
-    "f2a_phone_2",
-    "f2a_phone_number_2"
-  ];
+export class PreciseHomeComponent implements OnInit {
+  participants: any[];
+  columns = ["f2a_participant_id"];
+  dataSource: MatTableDataSource<IParticipantSummary>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   constructor(
-    store: CommonStore,
+    private commonStore: CommonStore,
+    public store: PreciseStore,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    store.setProjectById("precise");
+  ) {}
+
+  ngOnInit(): void {
+    this.commonStore.setProjectById("precise");
+  }
+  /**
+   * Triggered as a reaction from component when new data available from store
+   * Load as a table data source.
+   */
+  setDatasource() {
+    this.dataSource = new MatTableDataSource(this.store.participantSummaries$);
+    this.dataSource.paginator = this.paginator;
   }
 
-  handleRowClick(row: IODkTableRowData) {
-    this.router.navigate([row._id], { relativeTo: this.route });
+  addRecord() {
+    console.log("adding record");
+    this.store.launchParticipantForm();
+  }
+
+  applyFilter($event) {}
+
+  handleRowClicked(row: IODkTableRowData) {
+    console.log("row clicked", row);
+    this.router.navigate([row.f2a_participant_id], { relativeTo: this.route });
   }
 }
