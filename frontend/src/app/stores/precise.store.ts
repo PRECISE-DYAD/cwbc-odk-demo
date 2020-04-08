bcad7b4b8930c5c67ed8ed3ac0d1ba71ce076407
chrismclarke <chris.m.clarke@live.co.uk>
2020-04-07 22:02:27 -0700

    participant load, edit, revision methods

--- frontend/src/app/stores/precise.store.ts
+++ frontend/src/app/stores/precise.store.ts
@@ -1,8 +1,10 @@
 import { observable, action, computed } from "mobx-angular";
 import { Injectable } from "@angular/core";
 import { OdkService } from "../services/odk/odk.service";
-import { reaction } from 'mobx';
-
+import { reaction } from "mobx";
+import { IODkTableRowData, ODK_META_EXAMPLE } from "../types/odk.types";
+import { NotificationService } from "../services/notification/notification.service";
+import { uuidv4 } from "../utils/guid";
 
 /**
  * The PreciseStore manages persisted data and operations across the entire application,
@@ -10,16 +12,19 @@ import { reaction } from 'mobx';
  */
 @Injectable()
 export class PreciseStore {
-  constructor(private odk: OdkService) {
+  constructor(
+    private odk: OdkService,
+    private notifications: NotificationService
+  ) {
     this.loadParticipants();
   }
   @observable allParticipantsHashmap: IParticipantsHashmap;
   @computed get allParticipants() {
-    return Object.values(this.allParticipantsHashmap)
+    return Object.values(this.allParticipantsHashmap);
   }
   @observable participantSummaries: IParticipantSummary[];
-  @observable activeParticipant: IParticipant
-  @observable dataLoaded = false
+  @observable activeParticipant: IParticipant;
+  @observable dataLoaded = false;
 
   /**
    * Called on initial load, pull full participant data from main table
@@ -29,52 +34,102 @@ export class PreciseStore {
     const participants = (await this.odk.getTableRows(
       tables.ALL_PARTICIPANTS
     )) as any;
-    const merged = this._mergeParticipantRows(participants)
+    const merged = this._mergeParticipantRows(participants);
     this.participantSummaries = Object.values(merged).map((p, i) =>
       this.generateParticipantSummary(p, i)
     );
     this.allParticipantsHashmap = merged;
-    this.dataLoaded = true
+    this.dataLoaded = true;
   }
 
   // used as part of router methods in web preview
   @action setActiveParticipantById(ptid: string) {
     // skip if already selected
-    if (this.activeParticipant && this.activeParticipant.f2a_participant_id === ptid) return
+    if (
+      this.activeParticipant &&
+      this.activeParticipant.f2a_participant_id === ptid
+    )
+      return;
     // ensure participants loaded, use a 'reaction' to subscribe to changes,
     // and unsubscribe (dispose) thereafter to avoid memory leak.
     if (!this.allParticipantsHashmap) {
-      return reaction(() => this.dataLoaded, (isLoaded, r) => {
-        if (isLoaded) {
-          r.dispose()
-          return this.setActiveParticipantById(ptid)
+      return reaction(
+        () => this.dataLoaded,
+        (isLoaded, r) => {
+          if (isLoaded) {
+            r.dispose();
+            return this.setActiveParticipantById(ptid);
+          }
         }
-      })
+      );
     }
-    this.activeParticipant = this.allParticipantsHashmap[ptid]
-  }
-
-  @action updateParticipant(index: number, value: any) {
-    this.allParticipants[index] = value;
-    this.participantSummaries[index] = this.generateParticipantSummary(
-      value,
-      index
-    );
+    this.activeParticipant = this.allParticipantsHashmap[ptid];
   }
 
   /**
-   *
-   * @param participantIndex - if choosing to edit an existing participant, specify here
+   * Wrapper around more generic launchForm method. Additionally
+   * creates globally unique identifier to associate with participant
+   * throughout all forms
    */
-  launchParticipantForm(participantIndex?: number) {
-    // TO CONFIRM - INCLUDE ODK META IN TYPES
+  addParticipant() {
     const tableId = tables.ALL_PARTICIPANTS;
     const formId = tables.ALL_PARTICIPANTS;
-    if (participantIndex) {
-      const rowId = this.allParticipants[participantIndex]._rowID;
-      return this.odk.editRowWithSurvey(tableId, rowId, formId);
+    const f2_guid = uuidv4();
+    console.log("f2_guid", f2_guid);
+    return this.launchForm(tableId, formId, null, { f2_guid });
+  }
+  /**
+   * When editing a participant also create a revision entry
+   */
+  async editParticipant(participant: IParticipant) {
+    // remove meta fields to create revision
+    const tableId = tables.ALL_PARTICIPANTS;
+    const formId = tables.ALL_PARTICIPANTS;
+    const rowId = participant._id;
+    const backup = this._stripOdkMeta(participant);
+    console.log("editing participant", participant);
+    // TODO - find ways to only create if doesn't already exist
+    // Perhaps checking ETags...
+    await this.odk.addRow(
+      "genInfoRevisions",
+      backup,
+      rowId,
+      (res) => {
+        this.notifications.showMessage("backup success");
+        console.log("res", res);
+        this.odk.editRowWithSurvey(tableId, rowId, formId);
+      },
+      (err) => {
+        this.notifications.handleError(err);
+      }
+    );
+  }
+  async getParticipantRevisions() {
+    const allRevisions = await this.odk.getTableRows<IParticipant>(
+      tables.ALL_PARTICIPANTS__REVISIONS
+    );
+    console.log("allRevs", allRevisions);
+    const participantRevs = allRevisions.filter(
+      (rows) => rows._guid === this.activeParticipant.guid
+    );
+    console.log("participantRevs", participantRevs);
+    return participantRevs;
+  }
+  /**
+   *
+   * @param editableEntry - include full row details to load data into survey for editing
+   * @param jsonMap - fields to prepopulate
+   */
+  launchForm(
+    tableId: string,
+    formId: string,
+    editableEntry?: IODkTableRowData,
+    jsonMap?: any
+  ) {
+    if (editableEntry) {
+      return this.odk.editRowWithSurvey(tableId, editableEntry._id, formId);
     }
-    return this.odk.addRowWithSurvey(tableId, formId);
+    return this.odk.addRowWithSurvey(tableId, formId, null, jsonMap);
   }
 
   /**
@@ -92,27 +147,39 @@ export class PreciseStore {
     return summary as IParticipantSummary;
   }
 
-
-
   /**
    * Legacy ODK data is split in 3 separate rows per participant
    * Function to merge and return hashmap of results
    * TODO - remove once using new table structures
    */
-  private _mergeParticipantRows(participants: IParticipant[]): IParticipantsHashmap {
-    const merged: IParticipantsHashmap = {}
-    participants.forEach(p => {
+  private _mergeParticipantRows(
+    participants: IParticipant[]
+  ): IParticipantsHashmap {
+    const merged: IParticipantsHashmap = {};
+    participants.forEach((p) => {
       if (!merged[p.f2a_participant_id]) {
-        merged[p.f2a_participant_id] = {} as any
+        merged[p.f2a_participant_id] = {} as any;
       }
       // avoid overwriting "" fields with falsy check
       Object.entries(p).forEach(([field, fieldValue]) => {
         if (fieldValue) {
-          merged[p.f2a_participant_id][field] = fieldValue
+          merged[p.f2a_participant_id][field] = fieldValue;
         }
-      })
-    })
-    return merged
+      });
+    });
+    return merged;
+  }
+
+  private _stripOdkMeta<T>(data: IODkTableRowData & T): T {
+    // TODO - decide on full meta to remove and move to odk methods
+    const stripped = { ...data };
+    const odkMetaFields = Object.keys(ODK_META_EXAMPLE);
+    odkMetaFields.forEach((field) => {
+      if (stripped.hasOwnProperty(field)) {
+        delete stripped[field];
+      }
+    });
+    return stripped as T;
   }
 }
 
@@ -121,11 +188,14 @@ export class PreciseStore {
  ********************************************************************************/
 // mapping to reference different table fields in case of changes
 const tables = {
-  ALL_PARTICIPANTS: "demoDataAll",
+  ALL_PARTICIPANTS: "genInfo",
+  ALL_PARTICIPANTS__REVISIONS: "genInfoRevisions",
+  LEGACY_DATA: "demoLegacyData",
 };
 // fields used in summary views and search
+// _guid used to uniquely identify participant across all forms
 const PARTICIPANT_SUMMARY_FIELDS = [
-  "_rowID",
+  "f2_guid",
   "f2a_participant_id",
   "f2a_full_name",
   "f2a_national_id",
@@ -142,10 +212,13 @@ const PARTICIPANT_SUMMARY_FIELDS = [
 // create a type from the list of fields above
 type IParticipantSummaryField = typeof PARTICIPANT_SUMMARY_FIELDS[number];
 
-// placeholder interface to reflect all fields available within participant table
-export type IParticipant = { [K in IParticipantSummaryField]: string };
+// placeholder interface to reflect all fields available within participant table and ODK meta
+export type IParticipant = IODkTableRowData &
+  {
+    [K in IParticipantSummaryField]: string;
+  };
 
 // summary contains partial participant
-export type IParticipantSummary = Partial<IParticipant>
+export type IParticipantSummary = Partial<IParticipant>;
 
-type IParticipantsHashmap = { [ptid: string]: IParticipant }
+type IParticipantsHashmap = { [ptid: string]: IParticipant };
