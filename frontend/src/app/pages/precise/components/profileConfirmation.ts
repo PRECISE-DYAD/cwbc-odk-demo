10c61806907c33f074abc80cac14cfea15e8b821
chrismclarke <chris.m.clarke@live.co.uk>
2020-04-30 16:54:28 -0700

    add profile confirmation

---
+++ frontend/src/app/pages/precise/components/profileConfirmation.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "precise-profile-confirmation",
+  template: `<div>Confirmation</div>`,
+})
+export class PreciseProfileConfirmation implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
