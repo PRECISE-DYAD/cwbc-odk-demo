05d934ff8ae4e2ddb8142d703f898532a673953d
chrismclarke <chris.m.clarke@live.co.uk>
2020-05-28 15:01:06 -0700

    add server asset upload scripts

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,12 +1,11 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-module.exports = { recFindByExt };
+module.exports = { recFindByExt, recFind };
 
 /**
  * find files by a given extension recursively, returning full paths
  * */
-
 async function recFindByExt(base, ext, files, result) {
   files = files || (await fs.readdir(base));
   result = result || [];
@@ -19,6 +18,24 @@ async function recFindByExt(base, ext, files, result) {
       if (file.split(".").pop() === ext) {
         result.push(newbase);
       }
+    }
+  }
+  return result;
+}
+
+/**
+ * find files recursively, returning full paths
+ * */
+async function recFind(base, files, result) {
+  files = files || (await fs.readdir(base));
+  result = result || [];
+  for (const file of files) {
+    const newbase = path.join(base, file);
+    if (fs.statSync(newbase).isDirectory()) {
+      const newFiles = await fs.readdir(newbase);
+      result = await recFind(newbase, newFiles, result);
+    } else {
+      result.push(newbase);
     }
   }
   return result;
