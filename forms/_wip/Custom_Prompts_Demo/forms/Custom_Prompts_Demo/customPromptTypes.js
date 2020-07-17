f7fc5d916098ce65018e9b67138368fd3bea875a
chrismclarke <chris.m.clarke@live.co.uk>
2020-07-17 14:03:14 -0700

    custom number, autocomplete, warning prompts

--- forms/_wip/Custom_Prompts_Demo/forms/Custom_Prompts_Demo/customPromptTypes.js
+++ forms/_wip/Custom_Prompts_Demo/forms/Custom_Prompts_Demo/customPromptTypes.js
@@ -1,19 +1,30 @@
-define(["promptTypes", "jquery", "underscore", "prompts"], function (
-  promptTypes,
-  $,
-  _
-) {
+define(["promptTypes", "jquery"], function (promptTypes, $) {
   return {
-    breathcounter: promptTypes.launch_intent.extend({
-      type: "breathcounter",
-      datatype: "breathcounter",
-      buttonLabel: {
-        default: "Launch breath counter",
-        hindi: "शुरू करना सांस की गिनती",
-      },
-      intentString: "change.uw.android.BREATHCOUNT",
-      extractDataValue: function (jsonObject) {
-        return jsonObject.result.value;
+    /*************************************************************************************************
+     * E.g. Select autocomplete prompt type
+     * use jquery-ui to provide an autocomplete search list to an input box with select options
+     * NOTE - does not work as requires jquery-ui which is not available to survey (would require custom survey build)
+     ************************************************************************************************* */
+    custom_select_autocomplete_jquery: promptTypes.input_type.extend({
+      afterRender: function () {
+        try {
+          const promptId = this.getPromptId();
+          const name = this.renderContext.name;
+          const columns = this.renderContext.metadata.kvMap.Column;
+          const choices = columns[name]._displayChoicesList;
+          const autocompleteOptions = choices.map(function (c) {
+            return {
+              value: c.data_value,
+              label: c.display.title.text,
+            };
+          });
+          // NOTE - requires jqueryUI lib import in app/system/survey/js/main.js
+          $("#slider-" + promptId).autocomplete({
+            source: autocompleteOptions,
+          });
+        } catch (error) {
+          console.error(error);
+        }
       },
     }),
   };
