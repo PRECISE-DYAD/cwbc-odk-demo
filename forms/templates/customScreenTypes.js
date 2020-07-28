bb453e07c9e6badda5835f46ed8723a96e4d3c53
chrismclarke <chris.m.clarke@live.co.uk>
2020-07-28 16:27:22 -0700

    add custom screen to fix scroll issues and example

--- forms/templates/customScreenTypes.js
+++ forms/templates/customScreenTypes.js
@@ -1,3 +1,21 @@
 define(["screenTypes"], function (screenTypes) {
-  return {};
+  /**
+   * Extend the standard ODK screen that renders all child prompts with modified
+   * afterRender function to apply timeout to fix bug where sometimes scroll position
+   * would not update correctly and just scroll to the top on more complex pages
+   */
+  screenTypes.custom_prompts_screen = screenTypes.screen.extend({
+    afterRender: function () {
+      var that = this;
+      $.each(that.activePrompts, function (idx, prompt) {
+        prompt.afterRender();
+      });
+      if (that.focusScrollPos) {
+        // use a timeout as sometimes the scroll gets interrupted if also handling recursive delegated events
+        setTimeout(() => {
+          $(window).scrollTop(that.focusScrollPos);
+        }, 0);
+      }
+    },
+  });
 });
