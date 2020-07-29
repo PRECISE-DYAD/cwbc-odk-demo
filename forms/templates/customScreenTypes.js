658ca38a9a8cbdfd0c0516c393a40d9122c175bf
chrismclarke <chris.m.clarke@live.co.uk>
2020-07-28 21:36:18 -0700

    attempt fix of focus loss issue

--- forms/templates/customScreenTypes.js
+++ forms/templates/customScreenTypes.js
@@ -3,13 +3,51 @@ define(["screenTypes"], function (screenTypes) {
    * Extend the standard ODK screen that renders all child prompts with modified
    * afterRender function to apply timeout to fix bug where sometimes scroll position
    * would not update correctly and just scroll to the top on more complex pages
+   * NOTE: Code mostly copied from `screens.js`
    */
   screenTypes.custom_prompts_screen = screenTypes.screen.extend({
     afterRender: function () {
       var that = this;
+      console.log("custom_log afterRender", this);
       $.each(that.activePrompts, function (idx, prompt) {
         prompt.afterRender();
       });
+      // Handle focus of next element
+      if (
+        that.$focusPromptTest !== null &&
+        that.$focusPromptTest !== undefined
+      ) {
+        var focusElementAttr = {
+          id: that.$focusPromptTest.attr("id"),
+          value: that.$focusPromptTest.attr("value"),
+          name: that.$focusPromptTest.attr("name"),
+        };
+
+        var focusElementString = that.$focusPromptTest
+          .get(0)
+          .tagName.toLowerCase();
+        for (var key in focusElementAttr) {
+          if (focusElementAttr[key]) {
+            focusElementString =
+              focusElementString +
+              "[" +
+              key +
+              "='" +
+              focusElementAttr[key] +
+              "']";
+            setFocus = true;
+          }
+        }
+
+        if (setFocus === true) {
+          odkCommon.log(
+            "D",
+            "screens.afterRender: focusElementString = " + focusElementString
+          );
+          $(focusElementString).focus();
+        }
+      }
+      // Handle scroll position
       if (that.focusScrollPos) {
         // use a timeout as sometimes the scroll gets interrupted if also handling recursive delegated events
         setTimeout(() => {
