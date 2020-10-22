4977c982a60c565886a38895e9f3caa944f51894
Chris <chris.m.clarke@live.co.uk>
2020-10-23 00:01:18 +0100

    add dynamic .env select to export script

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,6 +1,29 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as inquirer from "inquirer";
+import * as dotenv from "dotenv";
+
+/**
+ * Read any files containing a .env in the filename and
+ * present as a list with ODK_SERVER_URL entries for selection
+ * and population to process.env
+ */
+export async function setEnv() {
+  const envFiles = fs
+    .readdirSync(process.cwd())
+    .filter((f) => f.includes(".env"));
+  const parsedEnvFiles = envFiles.map((f) => {
+    const e = dotenv.parse(fs.readFileSync(f));
+    return { name: `${path.basename(f)}: ${e.ODK_SERVER_URL}`, value: e };
+  });
+  const selectedEnv = await promptOptions(
+    parsedEnvFiles,
+    "Select Environment (.env file) to use"
+  );
+  Object.keys(selectedEnv).forEach((k) => {
+    process.env[k] = selectedEnv[k];
+  });
+}
 
 /**
  * find files by a given extension recursively, returning full paths
@@ -64,4 +87,18 @@ export async function promptOptions(
     { type: "list", name: "selected", message, choices },
   ]);
   return res.selected;
+}
+
+/**
+ * Take an input file and return an array of strings representing each line
+ */
+function readFileByLine(filepath: string) {
+  return (
+    fs
+      .readFileSync(filepath)
+      .toString()
+      // handle line breaks for windows and linux
+      .replace(/\r\n/g, "\n")
+      .split("\n")
+  );
 }
