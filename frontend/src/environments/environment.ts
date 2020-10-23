f31dd2e351c8123f98a1145f1f519de790e77f40
Chris <chris.m.clarke@live.co.uk>
2020-10-23 02:22:03 +0100

    update development environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,16 +5,23 @@ import { IEnvironment } from "src/app/types/types";
  * Modify to match any of the site-specific environments for testing
  */
 export const environment: IEnvironment = {
-  SITE: "gambia",
-  COUNTRY_CODE: "220",
+  SITE: "kenya",
+  COUNTRY_CODE: "254",
   production: false,
-  SENTRY_DSN: "",
+  SENTRY_DSN:
+    "https://6e0349ddbf724d77bce8b5adef4cbf4a@o443859.ingest.sentry.io/5418172",
   tableMapping: {
     Visit1: "Visit1_v2",
     Visit2: "Visit2_v2",
+    TOD_ANC: "TOD_ANC_v2",
+    Birthmother: "Birthmother_v2",
+    Postpartum_mother: "Postpartum_mother_v2",
   },
   formMapping: {
     Visit1: "Visit1_v2",
     Visit2: "Visit2_v2",
+    TOD_ANC: "TOD_ANC_v2",
+    Birthmother: "Birthmother_v2",
+    Postpartum_mother: "Postpartum_mother_v2",
   },
 };
