20b0d08f6f1027b7a82fe0775fd9a1cd75acb86a
Chris <chris.m.clarke@live.co.uk>
2020-11-12 13:32:59 +0000

    add dyad participant lists and bindings

--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -4,8 +4,7 @@ export const environment: IEnvironment = {
   SITE: "kenya",
   COUNTRY_CODE: "254",
   production: true,
-  SENTRY_DSN:
-    "https://6e0349ddbf724d77bce8b5adef4cbf4a@o443859.ingest.sentry.io/5418172",
+  SENTRY_DSN: "",
   tableMapping: {
     Visit1: "Visit1_v2",
     Visit2: "Visit2_v2",
