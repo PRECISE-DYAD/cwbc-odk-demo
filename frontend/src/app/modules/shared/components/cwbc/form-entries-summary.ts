1b47bb51856b920ac5cc9c5dfd8248aa70d20799
chrismclarke <chris.m.clarke@live.co.uk>
2021-02-26 14:22:51 -0800

    add checkpoint handling

--- frontend/src/app/modules/shared/components/cwbc/form-entries-summary.ts
+++ frontend/src/app/modules/shared/components/cwbc/form-entries-summary.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { IFormMetaWithEntries } from "src/app/modules/shared/types";
 
 /**
@@ -8,12 +8,16 @@ import { IFormMetaWithEntries } from "src/app/modules/shared/types";
 @Component({
   selector: "app-form-entries-summary",
   template: `
+    <!-- Existing entries -->
     <div *ngFor="let entry of form.entries">
       <button mat-button (click)="entrySelected.next(entry._id)" [disabled]="form._disabled">
+        <!-- The _savepoint_type (metadata) column === "COMPLETE" if they are 'official' values.
+         Otherwise, _savepoint_type === "INCOMPLETE" indicates a manual user savepoint and
+          _savepoint_type IS NULL indicates an automatic (checkpoint) savepoint. -->
         <ng-container [ngSwitch]="entry._savepoint_type">
           <mat-icon *ngSwitchCase="'COMPLETE'"> check_box</mat-icon>
           <mat-icon *ngSwitchCase="'INCOMPLETE'">indeterminate_check_box</mat-icon>
-          <mat-icon *ngSwitchDefault> check_box_outline_blank</mat-icon>
+          <mat-icon *ngSwitchDefault>update</mat-icon>
         </ng-container>
         <span>{{ title || form.title }}</span>
       </button>
@@ -31,7 +35,16 @@ import { IFormMetaWithEntries } from "src/app/modules/shared/types";
         <mat-icon>check_box_outline_blank</mat-icon>
         <span>{{ title || form.title }}</span>
       </button>
+      <div *ngIf="!entry._savepoint_type" style="">
+        <div class="inline-warning">
+          <span
+            >This entry has unsaved changes. Please re-open and either finalize, save or ignore the
+            changes</span
+          >
+        </div>
+      </div>
     </div>
+    <!-- First entry -->
     <button
       mat-button
       *ngIf="!form.allowRepeats && !form.entries[0]"
@@ -41,6 +54,7 @@ import { IFormMetaWithEntries } from "src/app/modules/shared/types";
       <mat-icon>check_box_outline_blank</mat-icon>
       <span>{{ title || form.title }}</span>
     </button>
+    <!-- Repeat entries -->
     <button
       mat-button
       *ngIf="form.allowRepeats"
@@ -50,13 +64,14 @@ import { IFormMetaWithEntries } from "src/app/modules/shared/types";
       <mat-icon>add</mat-icon>
       <span>{{ title || form.title }}</span>
     </button>
-    <div class="basic-warning" *ngIf="!form.allowRepeats && form.entries.length > 1">
+    <!-- Warnings and Messages -->
+    <div class="basic-warning" *ngIf="showMultipleEntryWarning">
       <mat-icon>warning</mat-icon>
       <span
         >Multiple entries detected. Please notify your supervisor or data manager to resolve.
       </span>
     </div>
-    <div *ngIf="form._disabled_msg" style="margin-left: 50px; font-style:italic; margin-top: 0px;">
+    <div *ngIf="form._disabled_msg" class="inline-warning">
       <!-- <mat-icon svgIcon="forms_required"></mat-icon> -->
       <span>{{ form._disabled_msg }}</span>
     </div>
@@ -75,9 +90,16 @@ import { IFormMetaWithEntries } from "src/app/modules/shared/types";
     `,
   ],
 })
-export class FormEntriesSummaryComponent {
+export class FormEntriesSummaryComponent implements OnInit {
   @Input() form: IFormMetaWithEntries;
   /** optional override of form title */
   @Input() title: string;
   @Output() entrySelected = new EventEmitter<string | undefined>();
+  showMultipleEntryWarning = false;
+
+  ngOnInit() {
+    // show multiple entries warning if multiple entries exist with savepoints (null value is automatic checkpoint, handled elsewhere)
+    const savedEntries = this.form.entries.filter((entry) => entry._savepoint_type);
+    this.showMultipleEntryWarning = !this.form.allowRepeats && savedEntries.length > 1;
+  }
 }
